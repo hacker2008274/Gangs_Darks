369c3c5ab37f27416f11052f6c47ce20d1a141c1
hacker2008274 <99984318+hacker2008274@users.noreply.github.com>
2022-07-28 02:49:06 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,24 @@
+echo -e "\033[4;91m[+] Starting setting up ...\033[0m "
+sleep 5
+echo -e "\033[4;91m[+] installing figlet ...\033[0m"
+sleep 5
+pkg install figlet
+echo "[+] installing nmap ..."
+pkg install nmap
+echo "[+] installing python2 ..."
+pkg install python2
+echo "[+] installing perl ..."
+pkg install perl
+echo -e "\033[4;91m[*] facebook : https://www.facebook.com/cyberviesociety186388252799292\033[0m"                                                                               sleep 5
+echo -e "\033[4;91m[•] We are Cyber VieSociety - Hackers VietNamese \033[0m"
+sleep 5
+echo -e "\033[4;91m[$] telegram : 0978842516 \033[0m"
+sleep 5
+echo -e "\033[4;91m[$] momo : 0978842516 \033[0m"
+sleep 3
+echo -e "\033[4;91m[$] Agribank : 8800205311040 \033[0m"
+sleep 3
+echo -e "\033[4;91m[©] i am Pham Chien\033[0m"
+sleep 3
+chmod +x *
+echo -e "\033[4;91m[!] command './viesociety.sh' run tools , thanks you :3 \033[0m"
